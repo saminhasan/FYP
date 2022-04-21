83a93a5af27fd316082f5ad94d0fb12dc5367d7e
Saminhasan5220 <saminhasan@iut-dhaka@edu.com>
2022-04-21 08:35:34 +0600

    .

--- sim.py
+++ sim.py
@@ -1,14 +1,97 @@
-
-
 from math import cos, sin
-import numpy as np
-from Quadrotor import Quadrotor
-from TrajectoryGenerator import TrajectoryGenerator
-from mpl_toolkits.mplot3d import Axes3D
+from numpy import array, sqrt, matmul, linalg
 from scipy import integrate
-import matplotlib.pyplot as plt
 
-show_animation = False
+class Quadrotor():
+	def __init__(self, x=0, y=0, z=0, roll=0, pitch=0, yaw=0):
+
+		self.x_data = []
+		self.y_data = []
+		self.z_data = []
+
+		self.update_pose(x, y, z, roll, pitch, yaw)
+
+	def update_pose(self, x, y, z, roll, pitch, yaw):
+		self.x = x
+		self.y = y
+		self.z = z
+		self.roll = roll
+		self.pitch = pitch
+		self.yaw = yaw
+		self.x_data.append(x)
+		self.y_data.append(y)
+		self.z_data.append(z)
+		
+		
+class TrajectoryGenerator():
+	def __init__(self, start_pos, des_pos, T, start_vel=[0,0,0], des_vel=[0,0,0], start_acc=[0,0,0], des_acc=[0,0,0]):
+		self.start_x = start_pos[0]
+		self.start_y = start_pos[1]
+		self.start_z = start_pos[2]
+
+		self.des_x = des_pos[0]
+		self.des_y = des_pos[1]
+		self.des_z = des_pos[2]
+
+		self.start_x_vel = start_vel[0]
+		self.start_y_vel = start_vel[1]
+		self.start_z_vel = start_vel[2]
+
+		self.des_x_vel = des_vel[0]
+		self.des_y_vel = des_vel[1]
+		self.des_z_vel = des_vel[2]
+
+		self.start_x_acc = start_acc[0]
+		self.start_y_acc = start_acc[1]
+		self.start_z_acc = start_acc[2]
+
+		self.des_x_acc = des_acc[0]
+		self.des_y_acc = des_acc[1]
+		self.des_z_acc = des_acc[2]
+
+		self.T = T
+
+	def solve(self):
+		A = array(
+			[[0, 0, 0, 0, 0, 1],
+			 [self.T**5, self.T**4, self.T**3, self.T**2, self.T, 1],
+			 [0, 0, 0, 0, 1, 0],
+			 [5*self.T**4, 4*self.T**3, 3*self.T**2, 2*self.T, 1, 0],
+			 [0, 0, 0, 2, 0, 0],
+			 [20*self.T**3, 12*self.T**2, 6*self.T, 2, 0, 0]
+			])
+
+		b_x = array(
+			[[self.start_x],
+			 [self.des_x],
+			 [self.start_x_vel],
+			 [self.des_x_vel],
+			 [self.start_x_acc],
+			 [self.des_x_acc]
+			])
+
+		b_y = array(
+			[[self.start_y],
+			 [self.des_y],
+			 [self.start_y_vel],
+			 [self.des_y_vel],
+			 [self.start_y_acc],
+			 [self.des_y_acc]
+			])
+
+		b_z = array(
+			[[self.start_z],
+			 [self.des_z],
+			 [self.start_z_vel],
+			 [self.des_z_vel],
+			 [self.start_z_acc],
+			 [self.des_z_acc]
+			])
+
+		self.x_c = linalg.solve(A, b_x)
+		self.y_c = linalg.solve(A, b_y)
+		self.z_c = linalg.solve(A, b_z)
+
 
 # Simulation parameters
 g = 9.81
@@ -32,16 +115,16 @@ Kd_y = 10
 Kd_z = 1
 
 
-def quad_sim(w0,x_c, y_c, z_c, run): # this looks like the outpuyt of the trajectory planner, trajectory planner takes wayopints as input.
+def quad_sim(x_c, y_c, z_c, wp): # this looks like the outpuyt of the trajectory planner, trajectory planner takes wayopints as input.
 	"""
 	Calculates the necessary thrust and torques for the quadrotor to
 	follow the trajectory described by the sets of coefficients
 	x_c, y_c, and z_c.
 	"""
-	#initial state?
-	x_pos = w0[1]
-	y_pos = w0[1]
-	z_pos = w0[2]
+	run = len(wp)
+	x_pos = wp[0][1]
+	y_pos = wp[0][1]
+	z_pos = wp[0][2]
 	x_vel = 0
 	y_vel = 0
 	z_vel = 0
@@ -57,29 +140,24 @@ def quad_sim(w0,x_c, y_c, z_c, run): # this looks like the outpuyt of the trajec
 
 	des_yaw = 0
 
-	dt = 0.1
+	dt = 0.11
 	t = 0
 
-	q = Quadrotor(x=x_pos, y=y_pos, z=z_pos, roll=roll,pitch=pitch, yaw=yaw, size=1, show_animation=show_animation) # for visualization 
-
+	q = Quadrotor(x=x_pos, y=y_pos, z=z_pos, roll=roll,pitch=pitch, yaw=yaw)
 	i = 0
-	n_run = run - 1 # waypoints ?
+	n_run = run - 1 # waypoints 
 	irun = 0
 	ppp, ttt = [], []
 	while True:
 		while t <= T:
-			# des_x_pos = calculate_position(x_c[i], t)
-			# des_y_pos = calculate_position(y_c[i], t)
 			des_z_pos = calculate_position(z_c[i], t)
-			# des_x_vel = calculate_velocity(x_c[i], t)
-			# des_y_vel = calculate_velocity(y_c[i], t)
 			des_z_vel = calculate_velocity(z_c[i], t)
 			des_x_acc = calculate_acceleration(x_c[i], t)
 			des_y_acc = calculate_acceleration(y_c[i], t)
 			des_z_acc = calculate_acceleration(z_c[i], t)
 
 			thrust = m * (g + des_z_acc + Kp_z * (des_z_pos -z_pos) + Kd_z * (des_z_vel - z_vel))
-			power = thrust * np.sqrt(x_vel**2 + y_vel**2 + z_vel**2 )
+			power = thrust * sqrt(x_vel**2 + y_vel**2 + z_vel**2 )
 			ppp.append(power[0])
 			ttt.append(t)
 			roll_torque = Kp_roll * (((des_x_acc * sin(des_yaw) - des_y_acc * cos(des_yaw)) / g) - roll)
@@ -97,7 +175,7 @@ def quad_sim(w0,x_c, y_c, z_c, run): # this looks like the outpuyt of the trajec
 			yaw += yaw_vel * dt
 
 			R = rotation_matrix(roll, pitch, yaw)
-			acc = (np.matmul(R, np.array([0, 0, thrust.item()]).T) - np.array([0, 0, m * g]).T) / m
+			acc = (matmul(R, array([0, 0, thrust.item()]).T) - array([0, 0, m * g]).T) / m
 			x_acc = acc[0]
 			y_acc = acc[1]
 			z_acc = acc[2]
@@ -117,12 +195,9 @@ def quad_sim(w0,x_c, y_c, z_c, run): # this looks like the outpuyt of the trajec
 		irun += 1
 		if irun >= n_run:
 			break
-
-	#print("Done")
+	print(len(ttt))
 	energy_consumptioin =  integrate.cumtrapz(ppp, ttt, initial=0)[-1]
-	#print('energy consumption : ',energy_consumptioin)
-	#plt.plot(ttt,ppp)
-	#plt.show()
+
 	return energy_consumptioin
 
 
@@ -184,11 +259,10 @@ def rotation_matrix(roll, pitch, yaw):
 	Returns
 		3x3 rotation matrix as NumPy array
 	"""
-	return np.array(
-		[[cos(yaw) * cos(pitch), -sin(yaw) * cos(roll) + cos(yaw) * sin(pitch) * sin(roll), sin(yaw) * sin(roll) + cos(yaw) * sin(pitch) * cos(roll)],
-		 [sin(yaw) * cos(pitch), cos(yaw) * cos(roll) + sin(yaw) * sin(pitch) *
-		  sin(roll), -cos(yaw) * sin(roll) + sin(yaw) * sin(pitch) * cos(roll)],
-		 [-sin(pitch), cos(pitch) * sin(roll), cos(pitch) * cos(yaw)]
+	return array(
+		[[cos(yaw) * cos(pitch), -sin(yaw) * cos(roll) + cos(yaw) * sin(pitch) * sin(roll),		sin(yaw) * sin(roll) + cos(yaw) * sin(pitch) * cos(roll)],
+		 [sin(yaw) * cos(pitch), cos(yaw) * cos(roll) + sin(yaw) * sin(pitch) *sin(roll),	   -cos(yaw) * sin(roll) + sin(yaw) * sin(pitch) * cos(roll)],
+		 [-sin(pitch),			cos(pitch) * sin(roll),											cos(pitch) * cos(yaw)]
 		 ])
 
 
@@ -200,20 +274,24 @@ def energy_from_waypoints(wp):
 	x_coeffs = [[]for _ in range(len(wp))]
 	y_coeffs = [[]for _ in range(len(wp))]
 	z_coeffs = [[]for _ in range(len(wp))]
+	start_vel=[0,0,0]
+	des_vel=[0,0,0]
 	for i in range(len(wp) - 1):
 		#print(wp[i], wp[(i + 1) % len(wp)])
 
 		traj = TrajectoryGenerator(wp[i], wp[(i + 1) % len(wp)], T)
 		traj.solve()
+		start_vel = []
+		des_vel= []
 		x_coeffs[i] = traj.x_c
 		y_coeffs[i] = traj.y_c
 		z_coeffs[i] = traj.z_c
 
-	return 	quad_sim(wp[0],x_coeffs, y_coeffs, z_coeffs,len(wp))
+	return	quad_sim(x_coeffs, y_coeffs, z_coeffs,wp)
 
 
 if __name__ == "__main__":
-	show_animation = True
 
-	waypoints = [[0, 0, 0], [0, 0, 5], [5, 5, 5], [0, 0, 5]]
-	#print(energy_from_waypoints(waypoints))
+	#waypoints = [[0, 0, 0], [0, 0, 5], [5, 5, 5], [0, 0, 5]]
+	waypoints = [[0, 0, 0], [0, 0, 5], [5, 0, 5], [5, 5, 5],  [0, 5, 5], [0, 0, 5]]
+	print(energy_from_waypoints(waypoints))
--- util.py
+++ util.py
@@ -2,8 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import *
 from matplotlib import cm
-from drone_sim import energy_from_waypoints
+from sim import energy_from_waypoints
+import pyximport; pyximport.install()
 
+np.random.seed(0)
+obstacles = [[2,2,3,0.5], [4,5,6,1]] #x, y, z, r
+fig = plt.figure('3D plots')
+ax = plt.axes(projection='3d')
+ax.set_xlabel('X Axis')
+ax.set_ylabel('Y Axis')
+ax.set_zlabel('Z Axis')
+ax.set_title('3D plot Title')
+
+		
 def calc_length(curve):
 
 	curve = curve.reshape(int(curve.shape[0]//3), 3)
@@ -21,34 +32,65 @@ def calc_time(path):
 
 	
 def calc_energy_consumption(path):
+	
 	path = path.reshape((int(path.shape[0]//3), 3))
-	energy = energy_from_waypoints(path)
+	#energy = energy_from_waypoints(path)
 	
 	
-	return energy
+	return 0#energy
 	
+def calc_collision(path, obstacles):
+	path = path.reshape((int(path.shape[0]//3), 3))
 
+	collision_cost = 100
+	total_cost = 0
+	for i in range(len(path) -1):
+		for j in range(len(obstacles)):
+
+			p1 = path[i]
+			p2 = path[i + 1]
+
+			p3 = obstacles[j][0:3]
+			r = obstacles[j][3]
+
+			a = (p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 + (p2[2] - p1[2])**2
+			b = 2*((p2[0] - p1[0])*(p1[0] - p3[0]) + (p2[1] - p1[1])*(p1[1] - p3[1]) + (p2[2] - p1[2])*(p1[2] - p3[2]))
+			c = p3[0]**2 + p3[1]**2 + p3[2]**2 + p1[0]**2 + p1[1]**2 + p1[2]**2 - 2*(p3[0]*p1[0] + p3[1]*p1[1] + p3[2]*p1[2]) - r**2
+
+			D = b * b - 4 * a * c
+
+			#if D < 0:
+				#print('no intersection')
+			#if D == 0:
+				#print('tangent')
+			if D > 0:
+				#print('section')
+				total_cost += collision_cost
+	return total_cost
 	
 def cost_func(path):
 	
-
 	length = calc_length(path)
 	length_weight = 1.0
 	length_cost = length * length_weight
 
-	vel = 10 
-	time = length/ vel 
+	vel = 10  # m/s
+	time = length / vel 
 	time_weight = 1.0
 	time_cost = time * time_weight
 	
 	
 	
 	energy_consumption = calc_energy_consumption(path)
-	energy_weight = 2.0
+	energy_weight = 1.0
 	energy_cost = energy_consumption * energy_weight
 	
-	#print(length_cost, time_cost, energy_cost)
-	cost = length_cost + time_cost + energy_cost
+	
+	collision = calc_collision(path,obstacles)
+	collision_weight = 1.0
+	collision_cost = collision * collision_weight
+	#print(length_cost, time_cost, energy_cost, collision_cost)
+	cost = length_cost + time_cost + energy_cost + collision_cost
 
 
 	return cost
@@ -58,13 +100,15 @@ def cost_func(path):
 def optimize_path(path):
 
 	bnds = calc_bounds(path)
-	nm = "Nelder-Mead"
-	pw = "Powell"
-	lb = "L-BFGS-B"
-	tnc = "TNC"
+	nm = "Nelder-Mead" # 100
+	pw = "Powell" # 120
+	lb = "L-BFGS-B" #19
+	tnc = "TNC" # 
 	tr="trust-constr" 
 	sl = "SLSQP"
-	result = minimize(cost_func, path, method=nm, bounds=bnds)
+	result = minimize(cost_func, path, method=nm, bounds=bnds ,options={'maxiter':len(path) * 3333 * 2 , 'adaptive': True, 'disp': True })
+	#result = minimize(fun, x0, args=(), method='Nelder-Mead', bounds=None, tol=None, callback=None, options={'func': None, 'maxiter': None, 'maxfev': None, 'disp': False, 'return_all': False, 'initial_simplex': None, 'xatol': 0.0001, 'fatol': 0.0001, 'adaptive': False})
+	
 	#result = minimize(cost_func, path, method=sl)
 	#result = brute(cost_func, path)
 	print(result.success)
@@ -97,19 +141,26 @@ def calc_bounds(path):
 	return bounds
 	
 def plot(initial_path, path):
-		fig = plt.figure('3D plots')
-		ax = plt.axes(projection='3d')
-		ax.set_xlabel('X Label')
-		ax.set_ylabel('Y Label')
-		ax.set_zlabel('Z Label')
-		ax.set_title('3D plot Title')
 		ax.scatter(initial_path[:,0],initial_path[:,1],initial_path[:,2], c = initial_path[:,2])
 		ax.plot(initial_path[:,0],initial_path[:,1],initial_path[:,2],label='Initial path')
 		ax.scatter(path[:,0],path[:,1],path[:,2], c = np.arange(0,len(path), 1))
 		ax.plot(path[:,0],path[:,1],path[:,2],label='Optimized path')
+		plot_obstacles(obstacles)
 		plt.legend()
 		plt.show()
-	
+
+
+def plot_obstacles(obstacles):
+	for j in range(len(obstacles)):
+		u = np.linspace(0, 2 * np.pi, 100)
+		v = np.linspace(0, np.pi, 100)
+		r = obstacles[j][3]
+		x = r * np.outer(np.cos(u), np.sin(v)) + obstacles[j][0]
+		y = r * np.outer(np.sin(u), np.sin(v)) + obstacles[j][1]
+		z = r * np.outer(np.ones(np.size(u)), np.cos(v)) + obstacles[j][2]
+
+		# Plot the surface
+		ax.plot_surface(x, y, z, color='b', alpha=0.1)
 def main():
 	'''
 	genarate a straight line path as initial solution.
@@ -118,11 +169,11 @@ def main():
 	
 	'''
 
-	step_size = 0.5 # in meters
+	step_size = 1.0 # in meters
 	# starting co-ordinate 
 	start = np.array([0,0,0]) 
 	# Finishing co-ordinate 
-	finish = np.array([0,0,20])
+	finish = np.array([5,5,5])
 	# shorttest path distance,  straight line path
 	linear_distance = np.linalg.norm(finish-start)
 	#calculate number of steps to divide path into
@@ -130,14 +181,16 @@ def main():
 
 	# The guess path for the optimizer to start workinig with, a straight line connecting start and finish line
 	initial_path = np.linspace(start, finish, num=int(num_steps), endpoint= True)
-	noise = np.random.randn(*initial_path.shape) * 0.2
+	noise = np.random.randn(*initial_path.shape) * 0.5
 	initial_path +=  noise
 	#print(initial_path, noise)
 	path = optimize_path(initial_path)
 	#reshapinig  output array  for plotttinig
 	#print(path, path.shape)
-
+	c = cost_func(path)
+	#print(c)
 	path = path.reshape((int(path.shape[0]//3), 3))
+	#print(path)
 
 	# plotting 
 	
@@ -145,11 +198,11 @@ def main():
 	
 	
 	# cost of stragith line path
-	#c = cost_func(initial_path)
-	#print(c)
+
 	#print(linear_distance, c)
 	#print(path,"\n","\n",len(path),"\n",np.diff(path,axis=0))
 	
 if __name__ == "__main__":
-	main()
+	for _ in range(1):
+		main()
 
